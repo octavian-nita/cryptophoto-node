f9618b55a0ba5f1ce966e68d5cd0b1f3ac04c06a
Octavian Nita <mnothro@yahoo.com>
2014-08-25 18:18:38 +0200

    v. 0.0.1

--- test/test-cryptophoto-node.js
+++ test/test-cryptophoto-node.js
@@ -3,15 +3,23 @@
 var cryptophoto = require('../lib/cryptophoto-node');
 
 cryptophoto.visibleIp(function(error, ip) {
-  var cpClient, cpSession;
+  var cpClient;
+
+  console.log('Visible IP: ' + ip);
+
   if (error) { return console.error(error.toString()); }
 
   cpClient = cryptophoto.createClient(process.env['CP_PUBLIC_KEY'] || 'efe925bda3bc2b5cd6fe3ad3661075a7',
                                       process.env['CP_PRIVATE_KEY'] || '384b1bda2dafcd909f607083da22fef0');
 
   cpClient.getSession('octavian', ip, function(error, session) {
-    if (error) { return console.error(error); }
+    if (error) { return console.error(error.toString()); }
 
-    console.log(session);
+    console.log(cpClient.getTokenGenerationWidget(session));
+
+    console.log(cpClient.getChallengeWidget(session));
+
+    cpClient.verify('selector', 'row', 'col', 'cph', 'octavian', ip, function(error, verification) {
+    });
   });
 });
