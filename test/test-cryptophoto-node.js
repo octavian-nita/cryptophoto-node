c846396b634e1761094800c068355ad270a613f6
octavian <octavian.nita@gmail.com>
2014-08-31 22:01:10 +0200

    v. 0.0.4: partial JSDoc + small refactoring

--- test/test-cryptophoto-node.js
+++ test/test-cryptophoto-node.js
@@ -1,16 +1,10 @@
+/**
+ * @todo Write proper tests / use a proper testing framework
+ */
+
 'use strict';
 
 var cryptophoto = require('../lib/cryptophoto-node');
-
-cryptophoto.proxy = {
-  host: '158.169.131.13',
-  port: 8012,
-  user: '*****',
-  pass: '*****',
-  headers: {
-    Cookie: 'BCIDSLB=*****'
-  }
-};
 
 cryptophoto.visibleIp(function(error, ip) {
   var cpClient;
@@ -19,8 +13,8 @@ cryptophoto.visibleIp(function(error, ip) {
 
   if (error) { return console.error(error.toString()); }
 
-  cpClient = cryptophoto.createClient(process.env['CP_PUBLIC_KEY'] || 'efe925bda3bc2b5cd6fe3ad3661075a7',
-                                      process.env['CP_PRIVATE_KEY'] || '384b1bda2dafcd909f607083da22fef0');
+  cpClient = cryptophoto.createClient(process.CP_PUBLIC_KEY || 'efe925bda3bc2b5cd6fe3ad3661075a7',
+                                      process.CP_PRIVATE_KEY || '384b1bda2dafcd909f607083da22fef0');
 
   cpClient.getSession('octavian', ip, function(error, session) {
     if (error) { return console.error(error.toString()); }
